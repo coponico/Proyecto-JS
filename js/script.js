577b3abbc7ade62d15affc8dd2f7d5a8d3523d0a
Nicolas Joli <nicolasjoli@yahoo.com>
2021-11-15 20:01:57 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var nombre = "Nico"
+var esAlmuno = true // o false
+var edad = 19
+var nulo = null
+var indefinido = undefined
+
+nombre = 25
+nombre = false
+
+console.log(nombre)
+console.log(edad)
+console.log(esAlmuno)
