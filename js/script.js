e55acef4c089aaae39fec69ff4000f9436d2476a
Nicolas Joli <nicolasjoli@yahoo.com>
2021-11-15 21:12:29 -0300

    Desafio 15/11

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-var nombre = "Nico"
-var esAlmuno = true // o false
-var edad = 19
-var nulo = null
-var indefinido = undefined
+let mensaje = prompt("Deja un mensaje:")
+let mensajeText = "Tu mensaje es:"
+let saludo = "¡Bienvenido! tu nombre es:"
+let nombre = prompt("Ingresa tu nombre")
+let apellido = prompt("Ahora el apellido pá")
+let edad = parseInt(prompt("¿Tu edad?"))
+let edadText = "Tu edad es"
+let numero1 = parseFloat(prompt("Número favorito"))
+let numeroText = "Tu número favorito es"
+let numero2 = 23
+let suerte1 = "si tu número favorito es"
+let suerte2 = "entonces tu número de la suerte es"
 
-nombre = 25
-nombre = false
+console.log( `${saludo} ${nombre} ${apellido}`)
+console.log(`${edadText} ${edad}`)
+console.log(`${numeroText} ${numero1}`)
+console.log(`${suerte1} ${numero1} ${suerte2} ${numero1 + numero2}` )
+alert(`${mensajeText} ${mensaje}`)
 
-console.log(nombre)
-console.log(edad)
-console.log(esAlmuno)
+
+// Operaciones y concatenaciones
+
