6af8bdb08f7492375bf28567b4fc6a8c6cdc6801
Nicolas Joli <nicolasjoli@yahoo.com>
2021-11-26 16:24:38 -0300

    Desafio 3

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
-// Saludo.
+let alimento1 = prompt("ingrese un alimento")
+let alimento2 = prompt("ingrese otro alimento")
+let alimento3 = prompt("ingrese uno más")
 
-let saludo = prompt("Saluda como es debido")
-if (saludo == "Hola") console.log("¡Que muchacho tan educado! Saludaste correctamente")
-else  console.warn("Te falta educación, no saludaste de manera adecuada.")
+const alimentos = [alimento1,alimento2,alimento3]
 
-
-// Número entre 10 y 50.
-
-let entre50 = prompt("Inserta un número")
-if (entre50 >= 10 && entre50 <= 50)  alert("Tu número se encuentra entre 10 y 50 y cumple con los requisitos.")
-else alert("El número es menor que 10 o mayor que 50.")
-
-
-// Número menor a 1000.
-
-let numero = prompt("Inserta otro número")
-if (numero <= 1000) alert("Tu número cumple los requisitos.") 
-else alert("Tu número no cumple con los requisitos (ser menor o igual que 1000)")
-
-
-
+console.log("Lista de compras:")
+for(var i = 0; i < alimentos.length; i++)
+{
+console.log(alimentos[i])
+}
