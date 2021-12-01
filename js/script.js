aeb8869e2702f311b6d9f23414934f1919f6b15a
Nicolas Joli <nicolasjoli@yahoo.com>
2021-12-01 15:32:46 -0300

    Desafio Objetos

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-let alimento1 = prompt("ingrese un alimento")
-let alimento2 = prompt("ingrese otro alimento")
-let alimento3 = prompt("ingrese uno más")
+const IVA = 1.21;
 
-const alimentos = [alimento1,alimento2,alimento3]
-
-console.log("Lista de compras:")
-for(var i = 0; i < alimentos.length; i++)
-{
-console.log(alimentos[i])
+class Producto {
+    constructor(nombre, precio, color, marca, stock) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.color = color;
+    this.marca = marca;
+    this.stock = stock;
 }
+
+precioFinal() {
+    let final = this.precio * IVA // Le agrega el desgraciado impuesto al producto
+    return final
+}
+}
+
+const producto1 = new Producto(prompt("Ingrese el nombre de un producto"), parseInt(prompt("Ingrese el precio")), prompt("Ingrese el color"), prompt("Ingrese la marca"), parseInt(prompt("Ingrese el stock")))
+
+console.log(producto1)
+console.log(`Precio final con IVA: ${producto1.precioFinal()}`)
+
