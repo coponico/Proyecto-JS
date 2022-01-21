a68afd3f87af69d35dcad94347d1a3c909c54f68
Nicolas Joli <nicolasjoli@yahoo.com>
2022-01-21 11:21:01 -0300

    AJAX y 3er Entrega

--- js/script.js
+++ js/script.js
@@ -5,224 +5,153 @@ iniciarSesion.addEventListener('click', (e) => {
     }
 })
 
-
-
-for (let autoEnArray of arrayAutos) {
-
-    console.log(autoEnArray)
-}
-
-let button1 = document.getElementById("sort1")
-let button2 = document.getElementById("sort2")
-let button3 = document.getElementById("sort3")
-let button4 = document.getElementById("sort4")
-
-button4.addEventListener('click', () => {
-    console.log("Solo precios", arrayAutos.map(arrayAutos => arrayAutos = arrayAutos.precio))
-})
-
-
-let productos = document.getElementById("Productos");
-
-arrayAutos.forEach(autoEnArray => {
-    let div = document.createElement('div')
-    div.classList.add('producto')
-    div.innerHTML += `
-    
-    <h4 class="productoNombre">${autoEnArray.marca} ${autoEnArray.modelo}</h4>
-    <img src="img/${autoEnArray.imagen}" alt="">
-    <p class="productoAño">Año: ${autoEnArray.año}</p>
-    <p class="productoPrecio">Precio: ${autoEnArray.precio}</p>
-    <div class="justifyProductos">
-    <button id="comprar">Comprar</button>
-    <button id="carrito">Añadir al carrito</button>
-    <label for="cuotas">Cuotas</label>
-    <select id="cuotas${autoEnArray.id}" class="cuotas" name="cuotas">
-        <option value="0"></option>
-        <option value="3">3 cuotas</option>
-        <option value="6">6 cuotas</option>
-        <option value="12">12 cuotas</option>
-    </select>
-    
-    <button type="button" id="producto${autoEnArray.id}" class="botonElegir">
-    Elegir
-    </button>
-        </div>
-    <div id="precioF${autoEnArray.id}" class="precioF"></div>
-        `
-    productos.appendChild(div)
-
-    let btnElegir = document.getElementById(`producto${autoEnArray.id}`)
-    btnElegir.addEventListener('click', () => {
-        elegir(autoEnArray.id)
-    })
-})
-
-button1.addEventListener("click", () => {
-    let menorAMayor = arrayAutos.sort((autoA, autoB) => autoA.precio - autoB.precio)
-    productos.innerHTML = "";
-    menorAMayor.forEach(autoEnArray => {
-        let div = document.createElement('div')
-        div.classList.add('producto')
-        div.innerHTML += `
+function comenzar() {
         
-        <h4 class="productoNombre">${autoEnArray.marca} ${autoEnArray.modelo}</h4>
-        <img src="img/${autoEnArray.imagen}" alt="">
-        <p class="productoAño">Año: ${autoEnArray.año}</p>
-        <p class="productoPrecio">Precio: ${autoEnArray.precio}</p>
-        <div class="justifyProductos">
-        <button id="comprar">Comprar</button>
-        <button id="carrito">Añadir al carrito</button>
-        <label for="cuotas">Cuotas</label>
-        <select id="cuotas${autoEnArray.id}" class="cuotas" name="cuotas">
-            <option value="0"></option>
-            <option value="3">3 cuotas</option>
-            <option value="6">6 cuotas</option>
-            <option value="12">12 cuotas</option>
-        </select>
         
-        <button type="button" id="producto${autoEnArray.id}" class="botonElegir">
-        Elegir
-        </button>
-            </div>
-        <div id="precioF${autoEnArray.id}" class="precioF"></div>
-            `
-        productos.appendChild(div)
-
-        let btnElegir = document.getElementById(`producto${autoEnArray.id}`)
-        btnElegir.addEventListener('click', () => {
-            elegir(autoEnArray.id)
-        })
-    })
-})
-
-button2.addEventListener('click', () => {
-    let mayorA1000 = arrayAutos.filter(autoEnArray => autoEnArray.precio >= 1000)
-    productos.innerHTML = "";
-    mayorA1000.forEach(autoEnArray => {
-
-        let div = document.createElement('div')
-        div.classList.add('producto')
-        div.innerHTML += `
-        <h4 class="productoNombre">${autoEnArray.marca} ${autoEnArray.modelo}</h4>
-        <img src="img/${autoEnArray.imagen}" alt="">
-        <p class="productoAño">Año: ${autoEnArray.año}</p>
-        <p class="productoPrecio">Precio: ${autoEnArray.precio}</p>
-        <div class="justifyProductos">
-        <button id="comprar">Comprar</button>
-        <button id="carrito">Añadir al carrito</button>
-        <label for="cuotas">Cuotas</label>
-        <select id="cuotas${autoEnArray.id}" class="cuotas" name="cuotas">
-            <option value="0"></option>
-            <option value="3">3 cuotas</option>
-            <option value="6">6 cuotas</option>
-            <option value="12">12 cuotas</option>
-        </select>
-        
-        <button type="button" id="producto${autoEnArray.id}" class="botonElegir">
-        Elegir
-        </button>
-            </div>
-        <div id="precioF${autoEnArray.id}" class="precioF"></div>
-            `
-        productos.appendChild(div)
-
-        let btnElegir = document.getElementById(`producto${autoEnArray.id}`)
-        btnElegir.addEventListener('click', () => {
-            elegir(autoEnArray.id)
-        })
-    })
-})
-
-const elegir = (id) => {
-
-    console.log(id)
-
-    let autoSeleccionado = arrayAutos.find(item => item.id == id)
-
-    let cuotasSeleccionadas = document.getElementById(`cuotas${id}`).value
-    if (cuotasSeleccionadas >= 1) {
-        let precioConCuotas = autoSeleccionado.precio / parseInt(cuotasSeleccionadas)
-        console.log(precioConCuotas);
-        let precioF = document.getElementById(`precioF${id}`)
-        precioF.innerHTML = `Precio final: $${parseInt(precioConCuotas)}`
-    } else {
-        let precioF = document.getElementById(`precioF${id}`)
-        precioF.innerHTML = `Precio final: $${parseInt(autoSeleccionado.precio)}`
-    }
-
-}
-
-// botonElegir.forEach(it => it.addEventListener("click", elegir))
-
-class Cliente {
-    constructor(nombre, apellido, dni, email, sueldo) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.email = email;
-        this.sueldo = sueldo;
-    }
-}
-
-let clientes = []
-
-if (localStorage.getItem('clientes')) {
-    clientes = JSON.parse(localStorage.getItem('clientes'))
-} else {
-    clientes = []
-}
-
-let formCliente = document.getElementById('formCliente')
-let divClientes = document.getElementById('divClientes')
-let parrafoError = document.getElementById('parrafoError')
-
-formCliente.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    let nombreCliente = document.getElementById("nombre").value
-    let apellidoCliente = document.getElementById("apellido").value
-    let dniCliente = document.getElementById("DNI").value
-    let emailCliente = document.getElementById("email").value
-    let sueldoCliente = document.getElementById("sueldo").value
-
-    let objetoCliente = new Cliente(nombreCliente, apellidoCliente, dniCliente, emailCliente, sueldoCliente)
-
-
-    clientes.push(objetoCliente)
-
-    localStorage.setItem('clientes', JSON.stringify(clientes))
-
-    formCliente.reset()
-})
-
-document.getElementById('botonClientes').addEventListener('click', () => {
-    let ClientesStorage = JSON.parse(localStorage.getItem('clientes'))
-
-    if (divClientes.children.length == 0) {
-        clientes.forEach((clienteArray, indice) => {
-            divClientes.innerHTML += `
-            <div id="cliente${indice}">
-            <p>${clienteArray.nombre}<p>
-            <button id="boton${indice}" class="btn btn-dark">Eliminar</button>
-            </div>
+        for (let autoEnArray of arrayAutos) {
             
-            `
+            console.log(autoEnArray)
+        }
+        
+        let button1 = document.getElementById("sort1")
+        let button2 = document.getElementById("sort2")
+        let button3 = document.getElementById("sort3")
+        let button4 = document.getElementById("sort4")
+        
+        button4.addEventListener('click', () => {
+            console.log("Solo precios", arrayAutos.map(arrayAutos => arrayAutos = arrayAutos.precio))
         })
-        ClientesStorage.forEach((clienteArray, indice) => {
-            document.getElementById(`boton${indice}`).addEventListener('click', () => {
-                document.getElementById(`cliente${indice}`).remove()
-                clientes.splice(indice, 1)
-                localStorage.setItem('clientes', JSON.stringify(clientes))
-                console.log(`el cliente ${clienteArray.nombre} ha sido eliminado`)
+        
+        
+        let productos = document.getElementById("Productos");
+        
+        arrayAutos.forEach(autoEnArray => {
+            let div = document.createElement('div')
+            div.classList.add('producto')
+            div.innerHTML += `
+            
+            <h4 class="productoNombre">${autoEnArray.marca} ${autoEnArray.modelo}</h4>
+            <img src="../img/${autoEnArray.imagen}" alt="">
+            <p class="productoAño">Año: ${autoEnArray.anio}</p>
+            <p class="productoPrecio">Precio: ${autoEnArray.precio}</p>
+            <div class="justifyProductos">
+            <button id="comprar">Comprar</button>
+            <button id="carrito${autoEnArray.id}" class="btn-carrito">Añadir al carrito</button>
+            <label for="cuotas">Cuotas</label>
+            <select id="cuotas${autoEnArray.id}" class="cuotas" name="cuotas">
+            <option value="0"></option>
+            <option value="3">3 cuotas</option>
+            <option value="6">6 cuotas</option>
+            <option value="12">12 cuotas</option>
+            </select>
+            
+            <button type="button" id="producto${autoEnArray.id}" class="botonElegir">
+        Elegir
+        </button>
+        </div>
+        <div id="precioF${autoEnArray.id}" class="precioF"></div>
+        `
+        productos.appendChild(div)
+        
+        let btnElegir = document.getElementById(`producto${autoEnArray.id}`)
+        btnElegir.addEventListener('click', () => {
+            elegir(autoEnArray.id)
+        })
+    })
+
+    button1.addEventListener("click", () => {
+        let menorAMayor = arrayAutos.sort((autoA, autoB) => autoA.precio - autoB.precio)
+        productos.innerHTML = "";
+        menorAMayor.forEach(autoEnArray => {
+            let div = document.createElement('div')
+            div.classList.add('producto')
+            div.innerHTML += `
+            
+            <h4 class="productoNombre">${autoEnArray.marca} ${autoEnArray.modelo}</h4>
+            <img src="img/${autoEnArray.imagen}" alt="">
+            <p class="productoAño">Año: ${autoEnArray.anio}</p>
+            <p class="productoPrecio">Precio: ${autoEnArray.precio}</p>
+            <div class="justifyProductos">
+            <button id="comprar">Comprar</button>
+            <button id="carrito${autoEnArray.id}" class="btn-carrito">Añadir al carrito</button>
+            <label for="cuotas">Cuotas</label>
+            <select id="cuotas${autoEnArray.id}" class="cuotas" name="cuotas">
+            <option value="0"></option>
+                <option value="3">3 cuotas</option>
+                <option value="6">6 cuotas</option>
+                <option value="12">12 cuotas</option>
+                </select>
+                
+                <button type="button" id="producto${autoEnArray.id}" class="botonElegir">
+                Elegir
+                </button>
+                </div>
+                <div id="precioF${autoEnArray.id}" class="precioF"></div>
+                `
+                productos.appendChild(div)
+                
+                let btnElegir = document.getElementById(`producto${autoEnArray.id}`)
+                btnElegir.addEventListener('click', () => {
+                    elegir(autoEnArray.id)
+                })
             })
         })
+        
+        button2.addEventListener('click', () => {
+            let mayorA1000 = arrayAutos.filter(autoEnArray => autoEnArray.precio >= 1000)
+            productos.innerHTML = "";
+            mayorA1000.forEach(autoEnArray => {
+                
+                let div = document.createElement('div')
+                div.classList.add('producto')
+                div.innerHTML += `
+                <h4 class="productoNombre">${autoEnArray.marca} ${autoEnArray.modelo}</h4>
+                <img src="img/${autoEnArray.imagen}" alt="">
+                <p class="productoAño">Año: ${autoEnArray.año}</p>
+                <p class="productoPrecio">Precio: ${autoEnArray.precio}</p>
+                <div class="justifyProductos">
+                <button id="comprar">Comprar</button>
+                <button id="carrito${autoEnArray.id}" class="btn-carrito">Añadir al carrito</button>
+                <label for="cuotas">Cuotas</label>
+                <select id="cuotas${autoEnArray.id}" class="cuotas" name="cuotas">
+                <option value="0"></option>
+                <option value="3">3 cuotas</option>
+                <option value="6">6 cuotas</option>
+                <option value="12">12 cuotas</option>
+                </select>
+                
+                <button type="button" id="producto${autoEnArray.id}" class="botonElegir">
+                Elegir
+                </button>
+                </div>
+                <div id="precioF${autoEnArray.id}" class="precioF"></div>
+                `
+                productos.appendChild(div)
+                
+                let btnElegir = document.getElementById(`producto${autoEnArray.id}`)
+                btnElegir.addEventListener('click', () => {
+                    elegir(autoEnArray.id)
+                })
+            })
+        })
+        
+        const elegir = (id) => {
+            
+            console.log(id)
+            
+            let autoSeleccionado = arrayAutos.find(item => item.id == id)
+            
+            let cuotasSeleccionadas = document.getElementById(`cuotas${id}`).value
+            if (cuotasSeleccionadas >= 1) {
+                let precioConCuotas = autoSeleccionado.precio / parseInt(cuotasSeleccionadas)
+                console.log(precioConCuotas);
+            let precioF = document.getElementById(`precioF${id}`)
+            precioF.innerHTML = `Precio final: $${parseInt(precioConCuotas)}`
+        } else {
+            let precioF = document.getElementById(`precioF${id}`)
+            precioF.innerHTML = `Precio final: $${parseInt(autoSeleccionado.precio)}`
+        }
+        
     }
+}
 
-
-
-})
-
-$('#carrito').on('click', () => {
-    $('#modalCarrito').modal('toggle')
-})
--- js/stock.js
+++ js/stock.js
@@ -1,20 +1,34 @@
 class Auto {
-    constructor(modelo, marca, año, precio, imagen, id) {
+    constructor(modelo, marca, anio, precio, imagen, id) {
         this.modelo = modelo;
         this.marca = marca;
-        this.año = año;
+        this.anio = anio;
         this.precio = precio;
         this.imagen = imagen;
         this.id = id
     }
 }
-const auto1 = new Auto("Mobi", "Fiat", 2015, 3200, "fiatmobi.jpg", 001)
-const auto2 = new Auto("Uno", "Fiat", 2017, 999, "fiatuno.jpg", 002)
-const auto3 = new Auto("500", "Fiat", 2002, 2300, "fiat500.jpg", 003)
-const auto4 = new Auto("Argo", "Fiat", 2013, 1200, "fiatargo.jpg", 004)
-const auto5 = new Auto("Joy", "Chevrolet", 2021, 5000, "chevroletjoy.jpg", 005)
-const auto6 = new Auto("Mustang", "Ford", 2018, 6250, "fordmustang.jpg", 006)
-const auto7 = new Auto("Raptor", "Ford", 2020, 1200, "fordraptor.jpg", 007)
+// const auto1 = new Auto("Mobi", "Fiat", 2015, 3200, "fiatmobi.jpg", 001)
+// const auto2 = new Auto("Uno", "Fiat", 2017, 999, "fiatuno.jpg", 002)
+// const auto3 = new Auto("500", "Fiat", 2002, 2300, "fiat500.jpg", 003)
+// const auto4 = new Auto("Argo", "Fiat", 2013, 1200, "fiatargo.jpg", 004)
+// const auto5 = new Auto("Joy", "Chevrolet", 2021, 5000, "chevroletjoy.jpg", 005)
+// const auto6 = new Auto("Mustang", "Ford", 2018, 6250, "fordmustang.jpg", 006)
+// const auto7 = new Auto("Raptor", "Ford", 2020, 1200, "fordraptor.jpg", 007)
+let arrayAutos = []
 
-
-let arrayAutos = [auto1, auto2, auto3, auto4, auto5, auto6, auto7]
+$.getJSON('js/stock.json', function (data){
+    data.forEach(elemento => {
+        arrayAutos.push(
+            new Auto(
+                elemento.modelo,
+                elemento.marca,
+                elemento.anio,
+                elemento.precio,
+                elemento.imagen,
+                elemento.id
+                )
+        )
+    });
+    comenzar()
+})
